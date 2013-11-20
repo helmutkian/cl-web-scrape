e9a2d4cc4c589108e339a487c6d8026f65bd19d8
helmutkian <helmut.rohrbacher@gmail.com>
2013-11-19 16:22:26 -0800

    Made tag readers into accessors

--- tag-destruct.lisp
+++ tag-destruct.lisp
@@ -1,6 +1,6 @@
 ;;; ************************************************************
 ;;; This module defines an interface for destructuring LHTML
-;;; tags, providing readers for their content and attributes.
+;;; tags, providing accessors for their content and attributes.
 ;;; ************************************************************
 
 (in-package #:com.helmutkian.cl-web-scrape)
@@ -15,12 +15,21 @@
 	     (list-of-lists-p (second lhtml)))
     (second (assoc attrib (second lhtml)))))
 
+(defsetf tag-attrib (tag attrib lhtml) (val)
+  `(when (and (eql (first ,lhtml) ,tag)
+	      (list-of-lists-p (second ,lhtml)))
+     (setf (second (assoc ,attrib (second ,lhtml))) ,val)))
+	   
+
 ;;; ************************************************************
 ;;; <a ...> Tag
 ;;; ************************************************************
 
 (defun a-href (lhtml)
   (tag-attrib :a :href lhtml))
+
+(defun (setf a-href) (url lhtml)
+  (setf (tag-attrib :a :href lhtml) url))
 
 ;;; ************************************************************
 ;;; <img ...> Tag
@@ -29,5 +38,11 @@
 (defun img-src (lhtml)
   (tag-attrib :img :src lhtml))
 
+(defun (setf img-src) (url lhtml)
+  (setf (tag-attrib :img :src lhtml) url))
+
 (defun img-alt (lhtml)
   (tag-attrib :img :alt lhtml))
+
+(defun (setf img-alt) (url lhtml)
+  (setf (tag-attrib :img :alt lhtml) url))
