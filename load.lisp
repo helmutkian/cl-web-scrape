c677c48e9109d0998989311b86d42ba10e211433
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-01-07 15:06:00 -0800

    Fixed loader

--- load.lisp
+++ load.lisp
@@ -1,14 +1,9 @@
 (defpackage #:cl-web-scrape-loader
-  (:nicknames #:wsl)
+  (:nicknames #:wsload)
   (:use #:cl)
   (:export #:cl-web-scrape-load))
 
 (in-package #:cl-web-scrape-loader)
 
-(defun cl-web-scrape-load (path-str)
-  (mapc #'ql:quickload '("closure-html" "drakma"))
-  (load (pathname (concatenate 'string path-str "cl-web-scrape.asd")))
-  (setf asdf:*central-registry*
-	(nunion (list (pathname path-str)) asdf:*central-registry*))
-  (asdf:operate 'asdf:load-op "cl-web-scrape")
-  (setf *package* 'common-lisp-user))
+(defun load-system ()
+  (ql:quickload '(closure-html drakma cl-coop selenium cl-web-scrape)))
