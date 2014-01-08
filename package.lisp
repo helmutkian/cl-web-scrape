19d3a7381cba6e5542df24f0751c0d519640072e
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-01-07 17:43:14 -0800

    Forgot to update exported syms in package

--- package.lisp
+++ package.lisp
@@ -1,9 +1,11 @@
 (defpackage #:com.helmutkian.cl-web-scrape
   (:nicknames #:ws)
   (:use :cl)
-  (:export #:html->lhtml
-	   #:get-html
-	   #:get-lhtml
+  (:export #:source->dom
+	   #:get-raw-source
+	   #:get-raw-dom
+	   #:get-processed-source
+	   #:get-processed-dom
 	   #:find-all
 	   #:find-first
 	   #:tag-attrib
