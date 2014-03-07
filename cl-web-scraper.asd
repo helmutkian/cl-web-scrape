5df18e19f05af46a1f8e431d373edaa04220de51
Helmut K Rohrbacher <helmut.rohrbacher@gmail.com>
2014-03-06 23:08:10 -0800

    Ready to go public soon

---
+++ cl-web-scraper.asd
@@ -0,0 +1,4 @@
+(asdf:defsystem #:cl-web-scraper
+  :depends-on ("closure-html" "drakma")
+  :components
+    (:file "web-scraper"))
--- load.lisp
+++
@@ -1,9 +0,0 @@
-(defpackage #:cl-web-scrape-loader
-  (:nicknames #:wsload)
-  (:use #:cl)
-  (:export #:cl-web-scrape-load))
-
-(in-package #:cl-web-scrape-loader)
-
-(defun load-system ()
-  (ql:quickload '(closure-html cxml alexandria drakma cl-coop selenium split-sequence cl-web-scrape)))
